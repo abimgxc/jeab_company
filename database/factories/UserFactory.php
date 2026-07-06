f230ce93793b17fcc187cb6dc608e869b28b1e29
abimgxc <magana.glenda03@gmail.com>
2026-07-06 17:48:56 +0000

    Corregir factory para compatibilidad

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker::create(); // Esto fuerza la creación del generador
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
